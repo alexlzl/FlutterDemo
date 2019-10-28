858b890e65420f1cfcbd4db3125efa5eb54f0384
liuzhouliang <525688067@qq.com>
2019-10-28 20:10:22 +0800

    dart

--- dart1/lib/Isolate使用.dart
+++ dart1/lib/Isolate使用.dart
@@ -2,33 +2,31 @@ import 'dart:async';
 import 'dart:isolate';
 
 main(List<String> args) {
-
   start();
 
   print("start");
-
 }
 
 Isolate isolate;
 
-int i =1;
+int i = 1;
 
-
-void start()async{
+void start() async {
   //接收消息的主Isolate的端口
   final receive = ReceivePort();
-
+/*
+要创建Isolate，你可以使用spawn方法。必须为spawn方法提供一个带有单个参数的“入口点”方法。 通常，此参数表示隔离应用于发送回通知消息的端口:
+ */
   isolate = await Isolate.spawn(runTimer, receive.sendPort);
 
-  receive.listen((data){
+  receive.listen((data) {
     print("Reveive : $data ; i :$i");
   });
 }
 
-
-void runTimer(SendPort port){
-  int counter =0 ;
-  Timer.periodic(const Duration(seconds: 1), (_){
+void runTimer(SendPort port) {
+  int counter = 0;
+  Timer.periodic(const Duration(seconds: 1), (_) {
     counter++;
     i++;
     final msg = "nitification $counter";
