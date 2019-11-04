3ab75ab5d57ff254bbff28d1f8dc27559aba5e17
liuzhouliang <525688067@qq.com>
2019-11-04 16:54:25 +0800

    flutter

---
+++ flutter_demo3/lib/绘制棋盘.dart
@@ -0,0 +1,93 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: CustomPaintRoute(),
+    );
+  }
+}
+
+class CustomPaintRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomPaint(
+        size: Size(300, 300), //指定画布大小
+        painter: MyPainter(),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    double eWidth = size.width / 15;
+    double eHeight = size.height / 15;
+
+    //画棋盘背景
+    var paint = Paint()
+      ..isAntiAlias = true
+      ..style = PaintingStyle.fill //填充
+      ..color = Color(0x77cdb175); //背景为纸黄色
+    canvas.drawRect(Offset.zero & size, paint);
+
+    //画棋盘网格
+    paint
+      ..style = PaintingStyle.stroke //线
+      ..color = Colors.black87
+      ..strokeWidth = 1.0;
+
+    for (int i = 0; i <= 15; ++i) {
+      double dy = eHeight * i;
+      canvas.drawLine(Offset(0, dy), Offset(size.width, dy), paint);
+    }
+
+    for (int i = 0; i <= 15; ++i) {
+      double dx = eWidth * i;
+      canvas.drawLine(Offset(dx, 0), Offset(dx, size.height), paint);
+    }
+
+    //画一个黑子
+    paint
+      ..style = PaintingStyle.fill
+      ..color = Colors.black;
+    canvas.drawCircle(
+      Offset(size.width / 2 - eWidth / 2, size.height / 2 - eHeight / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint,
+    );
+
+    //画一个白子
+    paint.color = Colors.white;
+    canvas.drawCircle(
+      Offset(size.width / 2 + eWidth / 2, size.height / 2 - eHeight / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint,
+    );
+  }
+
+  //在实际场景中正确利用此回调可以避免重绘开销，本示例我们简单的返回true
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
