8402f66ce6345d54d07da3dbcc94539c553ffc9b
liuzhouliang <525688067@qq.com>
2019-11-05 09:55:24 +0800

    flutter

--- flutter_demo3/lib/绘制棋盘.dart
+++ flutter_demo3/lib/绘制棋盘.dart
@@ -11,15 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: CustomPaintRoute(),
@@ -30,12 +21,20 @@ class MyApp extends StatelessWidget {
 class CustomPaintRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomPaint(
-        size: Size(300, 300), //指定画布大小
-        painter: MyPainter(),
-      ),
-    );
+    return
+      Scaffold(
+        backgroundColor: Colors.green,
+        appBar: AppBar(),
+        body: CustomPaint(
+          size: Size(300, 300),
+          painter: MyPainter(),
+        ),
+      )
+//      child: CustomPaint(
+//        size: Size(300, 300), //指定画布大小
+//        painter: MyPainter(),
+//      ),
+    ;
   }
 }
 
