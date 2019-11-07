eb7f51512cfb4732e94cb5f31e66acf6e14cdddc
liuzhouliang <525688067@qq.com>
2019-11-07 14:39:02 +0800

    flutter

---
+++ dart1/lib/操作符重载.dart
@@ -0,0 +1,25 @@
+class Role {
+  final String name;
+  final int _accessLevel;
+
+  const Role(this.name, this._accessLevel);
+  bool operator >(Role Other) {
+    return this._accessLevel > Other._accessLevel;
+  }
+
+  bool operator <(Role Other) {
+    return this._accessLevel < Other._accessLevel;
+  }
+}
+
+main() {
+  var adminRole = new Role('管理员', 3);
+  var reporterRole = new Role('报告员', 2);
+  var userRole = new Role('用户', 1);
+  if (adminRole > reporterRole) {
+    print("管理员的权限大于报告员");
+  }
+  if (reporterRole > userRole) {
+    print("报告员的权限大于用户");
+  }
+}
